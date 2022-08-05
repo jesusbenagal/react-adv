ec3a16330c2cf3abd61c64e95a7c2f18efa71842
Jesús Guerrero <jesus.guerrero@talan.com>
2022-08-05 13:37:21 +0200

    Component compound listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 
 import logo from "../logo.svg";
 
@@ -12,10 +13,10 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to="/home"
+                to="/"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Home
+                Shopping
               </NavLink>
             </li>
             <li>
@@ -40,7 +41,7 @@ export const Navigation = () => {
         <Routes>
           <Route path="about" element={<h1>About</h1>} />
           <Route path="users" element={<h1>Users</h1>} />
-          <Route path="home" element={<h1>Home</h1>} />
+          <Route path="/" element={<ShoppingPage />} />
 
           <Route path="/*" element={<Navigate to="/home" replace />} />
         </Routes>
