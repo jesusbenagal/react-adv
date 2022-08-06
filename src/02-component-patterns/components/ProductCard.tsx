e52bc7c4bed7bfc2961b0cee51bec78fd5ad688c
Jesús Guerrero <jesus.guerrero@talan.com>
2022-08-06 09:28:40 +0200

    Component extend styles listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,22 +1,29 @@
-import { createContext, ReactElement, useContext } from "react";
+import { createContext, ReactElement, CSSProperties } from "react";
 
 import { useProduct } from "../hooks/useProduct";
-import {
-  ProductContextProps,
-  ProductCardProps,
-} from "../interfaces/interfaces";
+import { ProductContextProps } from "../interfaces/interfaces";
 
 import styles from "../styles/styles.module.css";
+import { Product } from "../interfaces/interfaces";
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product }: ProductCardProps) => {
+export interface Props {
+  children?: ReactElement | ReactElement[];
+  product: Product;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductCard = ({ children, product, className, style }: Props) => {
   const { counter, increaseBy } = useProduct();
 
   return (
     <Provider value={{ counter, increaseBy, product }}>
-      <div className={styles.productCard}>{children}</div>
+      <div className={`${styles.productCard} ${className}`} style={style}>
+        {children}
+      </div>
     </Provider>
   );
 };
--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -3,7 +3,13 @@ import { ProductContext } from "./ProductCard";
 import noImage from "../assets/no-image.jpg";
 import styles from "../styles/styles.module.css";
 
-export const ProductImage = ({ img = "" }) => {
+export interface Props {
+  className?: string;
+  img?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductImage = ({ img, className, style }: Props) => {
   const { product } = useContext(ProductContext);
 
   let imgToShow: string;
@@ -16,5 +22,12 @@ export const ProductImage = ({ img = "" }) => {
     imgToShow = noImage;
   }
 
-  return <img className={styles.productImg} src={imgToShow} alt="Product" />;
+  return (
+    <img
+      className={`${styles.productImg} ${className}`}
+      src={imgToShow}
+      alt="Product"
+      style={style}
+    />
+  );
 };
