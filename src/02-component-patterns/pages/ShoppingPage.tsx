c73a1f89ed2c0c156e006f94d5959df10e3edf0e
Jesús Guerrero <jesus.guerrero@talan.com>
2022-08-13 12:29:56 +0200

    Pre-NPM listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -3,7 +3,6 @@ import ProductCard, {
   ProductImage,
   ProductTitle,
 } from "../components/";
-import "../styles/custom-styles.css";
 import { products } from "../data/products";
 
 const product = products[0];
@@ -16,29 +15,18 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
-          maxCount: 10,
+          // maxCount: 10,
         }}
       >
         {({ reset, increaseBy, isMaxCountReached, count, maxCount }) => (
           <>
             <ProductImage
-              className="custom-image"
               style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
             />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-            <span>
-              {count} - {maxCount}
-            </span>
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
